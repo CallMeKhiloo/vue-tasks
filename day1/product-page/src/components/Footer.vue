a29535ec544838bc9b9238cffc48225e63980b50
CallMeKhiloo <khm40005000@outlook.com>
2026-05-22 19:38:32 +0300

    finished day1 tasks

---
+++ day1/product-page/src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer
+    class="footer footer-center p-4 bg-neutral text-neutral-content font-semibold shadow-inner mt-12"
+  >
+    <p>&copy; 2026 My App. All rights reserved.</p>
+  </footer>
+</template>
---
+++ day1/product-page/src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <header
+    class="navbar bg-neutral text-neutral-content justify-center font-bold text-xl py-4 shadow-md"
+  >
+    <h1 class="text-2xl font-bold">My App</h1>
+  </header>
+</template>
